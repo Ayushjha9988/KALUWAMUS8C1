7fffb7f826e4f2d8f8aa2b39ad8421ce27ff63f2
𓆩𝐀𝐘𝐔𝐒𝐇𓆪 ♡ 𝐗ᴅ🪽 <105988266+Ayushjha9988@users.noreply.github.com>
2025-08-04 19:35:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,8 +90,8 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg",
-                 "https://te.legra.ph/file/c15d01b3e6b40ea141dc9.jpg",
+START_IMG_URL = ["https://files.catbox.moe/3b0v4t.jpg",
+                 "https://files.catbox.moe/dwrc1r.jpg",
                  "https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg"]
     
 PING_IMG_URL = getenv(
